b2cbe13a9cd94e7eee1eb6624ec59567121fa6d5
Cklla <contact@cklla.ch>
2026-08-28 13:03:58 +0200

    feat(home): wire HomeScreen into MainActivity

--- app/src/main/java/com/openclassrooms/joiefull/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/joiefull/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.openclassrooms.joiefull.ui.home.HomeScreen
 import com.openclassrooms.joiefull.ui.theme.JoiefullTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JoiefullTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JoiefullTheme {
-        Greeting("Android")
-    }
-}
