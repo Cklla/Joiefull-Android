5a2b90aad42043078891f7edbeaa985e0df9a008
Cklla <contact@cklla.ch>
2026-08-28 13:03:42 +0200

    fix(data): remove stray space in clothes API endpoint path

--- app/src/main/java/com/openclassrooms/joiefull/data/remote/ClothesApiService.kt
+++ app/src/main/java/com/openclassrooms/joiefull/data/remote/ClothesApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface ClothesApiService {
 
-    @GET("OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main /api/clothes.json")
+    @GET("OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/main/api/clothes.json")
     suspend fun getArticles(): List<ArticleDto>
 }
